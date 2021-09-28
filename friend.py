4c0f86f8c8912530c9e960b663f1bf51df0d1bf4
Col-R <2colewestin@gmail.com>
2021-09-28 16:16:51 -0500

    added connections to establish database output

--- friend.py
+++ friend.py
@@ -9,15 +9,15 @@ class Friend:
         self.occupation = data['occupation']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
-    # Now we use class methods to query our database
+    # Now we use class methods to query our database
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM friends;"
-        # make sure to call the connectToMySQL function with the schema you are targeting.
+        # make sure to call the connectToMySQL function with the schema you are targeting.
         results = connectToMySQL('first_flask').query_db(query)
-        # Create an empty list to append our instances of friends
+        # Create an empty list to append our instances of friends
         friends = []
-        # Iterate over the db results and create instances of friends with cls.
+        # Iterate over the db results and create instances of friends with cls.
         for friend in results:
             friends.append( cls(friend) )
         return friends
