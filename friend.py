37a882e48e14c802e0257293da47fb1e6988a2c5
Col-R <2colewestin@gmail.com>
2021-09-28 17:13:27 -0500

    added other class method for taking form input

--- friend.py
+++ friend.py
@@ -12,6 +12,10 @@ class Friend:
     # Now we use class methods to query our database
     @classmethod
     def get_all(cls):
+        # query = "SELECT * FROM users WHERE email = %(email)s;"
+        # data = { 'email' : request.form['email'] }
+        # result = mysql.query_db(query, data)
+
         query = "SELECT * FROM friends;"
         # make sure to call the connectToMySQL function with the schema you are targeting.
         results = connectToMySQL('first_flask').query_db(query)
@@ -21,3 +25,9 @@ class Friend:
         for friend in results:
             friends.append( cls(friend) )
         return friends
+        
+    @classmethod
+    def save(cls, data ):
+        query = "INSERT INTO friends ( first_name , last_name , occupation , created_at, updated_at ) VALUES ( %(fname)s , %(lname)s , %(occ)s , NOW() , NOW() );"
+        # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL('first_flask').query_db( query, data )
